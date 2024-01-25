a917e9559ff6c7afdf3d0f1c1b3ddb5ea8d0a860
Eileen <eileenburdekin@proton.me>
2024-01-24 21:46:46 -0800

    add POST method to login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,35 +3,50 @@ import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 
 export default function Login() {
-  const [existingUser, setExistingUser] = useState({
+  const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
 
-  const handleExistingUserSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:5555/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const user = await response.json();
+        localStorage.setItem("userID", user.id); // Store user ID in local storage
+        // Redirect to the appropriate page or display a success message
+      } else {
+        // Handle signup error (e.g., display error message)
+      }
+    } catch (error) {
+      // Handle network errors
+    }
     // Handle existing user form submission logic here
     // Redirect to /login after submission
   };
 
   return (
     <Box>
-      <form onSubmit={handleExistingUserSubmit}>
+      <form onSubmit={handleSubmit}>
         <label>Email:</label>
         <input
           type="email"
-          value={existingUser.email}
-          onChange={(e) =>
-            setExistingUser({ ...existingUser, email: e.target.value })
-          }
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
           required
         />
         <label>Password:</label>
         <input
           type="password"
-          value={existingUser.password}
+          value={formData.password}
           onChange={(e) =>
-            setExistingUser({ ...existingUser, password: e.target.value })
+            setFormData({ ...formData, password: e.target.value })
           }
           required
         />
